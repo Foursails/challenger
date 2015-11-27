a37574e1f2493941efce000b2fd8f41015b0bb4d
Matthew James Davis <davis.matthewjames@gmail.com>
2015-11-27 15:41:42 +0900

    add controller support add styling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 export class Challenger {
 
+  gamepads;
+
+  active;
+
   constructor() {
+    this.gamepads = [];
+  }
+
+  activate() {
+    this.active = true;
+    this.watchGamepads();
+  }
+
+  deactivate() {
+    this.active = false;
+    this.gamepads = null;
+  }
+
+  // Begins a loop, via requestAnimationFrame, that listens to the attached
+  // gamepads.
+  watchGamepads() {
+    this.active && requestAnimationFrame(() => {
+      this.gamepads.splice(0, Infinity, ...navigator.getGamepads());
+      this.watchGamepads();
+    });
   }
 }
